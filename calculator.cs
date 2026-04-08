06933e1cf6a5f6d8d005f4e9b3d74295573160b5
kinanthi <l.kinanthi@students.amikom.ac.id>
2026-04-08 16:06:50 +0700

    menambahkan fungsi Pengurangan

--- calculator.cs
+++ calculator.cs
@@ -2,10 +2,14 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-
+		
 	}
 	static int Penamabahan(int a, int b)
 	{
-		return a + b:
+		return a + b;
+	}
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
